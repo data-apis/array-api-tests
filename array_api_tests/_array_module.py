d95abdf20f0ebebb1069a2040c3d2e00882945f9
Aaron Meurer <asmeurer@gmail.com>
2020-09-04 14:25:04 -0600

    Start file for parameterizing the array module

---
+++ array_api_tests/_array_module.py
@@ -0,0 +1,22 @@
+import os
+from importlib import import_module
+
+# Replace this with a specific array module to test it, for example,
+#
+# import numpy as array_module
+array_module = None
+
+if array_module is None:
+    if 'ARRAY_API_TESTS_MODULE' in os.environ:
+        mod_name = os.environ['ARRAY_API_TESTS_MODULE']
+        mod = import_module(mod_name)
+    else:
+        raise RuntimeError("No array module specified. Either edit _array_module.py or set the ARRAY_API_TESTS_MODULE environment variable")
+else:
+    mod = array_module
+
+# Names from the spec. This is what should actually be imported from this
+# file.
+
+array = mod.array
+dtype = mod.dtype
