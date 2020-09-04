cb0f024eb127afbccd291c0646004633d4bcd877
Aaron Meurer <asmeurer@gmail.com>
2020-09-04 12:58:03 -0600

    Add type promotion rules as dictionaries
    There are no tests for them yet. This is just copying what is in the spec into
    the file.

---
+++ array_api_tests/test_type_promotion.py
@@ -0,0 +1,74 @@
+"""
+https://github.com/data-apis/array-api/blob/master/spec/API_specification/type_promotion.md
+"""
+
+signed_integer_promotion_table = {
+    ('i1', 'i1'): 'i1',
+    ('i1', 'i2'): 'i2',
+    ('i1', 'i4'): 'i4',
+    ('i1', 'i8'): 'i8',
+    ('i2', 'i1'): 'i2',
+    ('i2', 'i2'): 'i2',
+    ('i2', 'i4'): 'i4',
+    ('i2', 'i8'): 'i8',
+    ('i4', 'i1'): 'i4',
+    ('i4', 'i2'): 'i4',
+    ('i4', 'i4'): 'i4',
+    ('i4', 'i8'): 'i8',
+    ('i8', 'i1'): 'i8',
+    ('i8', 'i2'): 'i8',
+    ('i8', 'i4'): 'i8',
+    ('i8', 'i8'): 'i8',
+}
+
+unsigned_integer_promotion_table = {
+    ('u1', 'u1'): 'u1',
+    ('u1', 'u2'): 'u2',
+    ('u1', 'u4'): 'u4',
+    ('u1', 'u8'): 'u8',
+    ('u2', 'u1'): 'u2',
+    ('u2', 'u2'): 'u2',
+    ('u2', 'u4'): 'u4',
+    ('u2', 'u8'): 'u8',
+    ('u4', 'u1'): 'u4',
+    ('u4', 'u2'): 'u4',
+    ('u4', 'u4'): 'u4',
+    ('u4', 'u8'): 'u8',
+    ('u8', 'u1'): 'u8',
+    ('u8', 'u2'): 'u8',
+    ('u8', 'u4'): 'u8',
+    ('u8', 'u8'): 'u8',
+}
+
+mixed_signed_unsigned_promotion_table = {
+    ('i1', 'u1'): 'i2',
+    ('i1', 'u2'): 'i4',
+    ('i1', 'u4'): 'i8',
+    ('i2', 'u1'): 'i2',
+    ('i2', 'u2'): 'i4',
+    ('i2', 'u4'): 'i8',
+    ('i4', 'u1'): 'i2',
+    ('i4', 'u2'): 'i4',
+    ('i4', 'u4'): 'i8',
+}
+
+flipped_mixed_signed_unsigned_promotion_table = {(u, i): p for (i, u), p in mixed_signed_unsigned_promotion_table.items()}
+
+float_promotion_table = {
+    ('f4', 'f4'): 'f4',
+    ('f4', 'f8'): 'f8',
+    ('f8', 'f4'): 'f8',
+    ('f8', 'f8'): 'f8',
+}
+
+promotion_table = {
+    **signed_integer_promotion_table,
+    **unsigned_integer_promotion_table,
+    **mixed_signed_unsigned_promotion_table,
+    **flipped_mixed_signed_unsigned_promotion_table,
+    **float_promotion_table,
+}
+
+if __name__ == '__main__':
+    for (i, j), p in promotion_table.items():
+        print(f"({i}, {j}) -> {p}")
