b17179fb255e468636440f5333bd76a68c9fac85
Aaron Meurer <asmeurer@gmail.com>
2020-09-11 15:53:17 -0600

    Just use pytest.skip instead of pytest-dependency

--- array_api_tests/test_signatures.py
+++ array_api_tests/test_signatures.py
@@ -6,20 +6,14 @@ from ._array_module import mod, mod_name
 
 from .function_stubs import elementwise
 
-@pytest.mark.parametrize('name', [
-    pytest.param(name,
-                 marks=pytest.mark.dependency(name=f"{name}_exists"))
-    for name in elementwise._names])
+@pytest.mark.parametrize('name', elementwise._names)
 def test_has_names_elementwise(name):
     assert hasattr(mod, name), f"{mod_name} is missing the elementwise function {name}"
 
-
-@pytest.mark.parametrize('name', [
-    pytest.param(name,
-                 marks=pytest.mark.dependency(name=f"{name}_parameters",
-                                             depends=[f"{name}_exists"]))
-    for name in elementwise._names])
+@pytest.mark.parametrize('name', elementwise._names)
 def test_function_parameters(name):
+    if not hasattr(mod, name):
+        pytest.skip(f"{mod_name} does not have {name}, skipping.")
     stub_func = getattr(elementwise, name)
     mod_func = getattr(mod, name)
     signature = inspect.signature(stub_func)
