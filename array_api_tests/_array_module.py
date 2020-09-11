42b8a22e80bff8410cacc87686b6dff0ed011e2b
Aaron Meurer <asmeurer@gmail.com>
2020-09-11 15:31:59 -0600

    Add support for dotted module names like mxnet.nd in ARRAY_API_TESTS_MODULE

--- array_api_tests/_array_module.py
+++ array_api_tests/_array_module.py
@@ -9,7 +9,10 @@ array_module = None
 if array_module is None:
     if 'ARRAY_API_TESTS_MODULE' in os.environ:
         mod_name = os.environ['ARRAY_API_TESTS_MODULE']
-        mod = import_module(mod_name)
+        _module, _sub = mod_name.split('.', 1)
+        mod = import_module(_module)
+        if _sub:
+            mod = getattr(mod, _sub)
     else:
         raise RuntimeError("No array module specified. Either edit _array_module.py or set the ARRAY_API_TESTS_MODULE environment variable")
 else:
