0aff77bc0d4eb30b82c21fe052e3caeca2073f30
Aaron Meurer <asmeurer@gmail.com>
2020-09-11 15:49:35 -0600

    Proof of concept using pytest-dependency
    This can allow automatically skipping tests when a function does not exist.
    However, I'm not sure if it's OK to use a pytest plugin. Also, I'm not
    actually sure I like this. It's rather verbose, and doesn't seem much better
    than just
    
    if not hasattr(mod, name):
        skip()

--- array_api_tests/test_signatures.py
+++ array_api_tests/test_signatures.py
@@ -1,4 +1,25 @@
+import inspect
+
+import pytest
+
 from ._array_module import mod, mod_name
 
+from .function_stubs import elementwise
+
+@pytest.mark.parametrize('name', [
+    pytest.param(name,
+                 marks=pytest.mark.dependency(name=f"{name}_exists"))
+    for name in elementwise._names])
 def test_has_names_elementwise(name):
     assert hasattr(mod, name), f"{mod_name} is missing the elementwise function {name}"
+
+
+@pytest.mark.parametrize('name', [
+    pytest.param(name,
+                 marks=pytest.mark.dependency(name=f"{name}_parameters",
+                                             depends=[f"{name}_exists"]))
+    for name in elementwise._names])
+def test_function_parameters(name):
+    stub_func = getattr(elementwise, name)
+    mod_func = getattr(mod, name)
+    signature = inspect.signature(stub_func)
