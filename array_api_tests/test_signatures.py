4292d252d7b270f7a3b579ed3fadb50af1ed9c1e
Aaron Meurer <asmeurer@gmail.com>
2020-09-11 16:43:45 -0600

    Add custom helper functions raises() and doesnt_raise()

--- array_api_tests/test_signatures.py
+++ array_api_tests/test_signatures.py
@@ -6,6 +6,27 @@ from ._array_module import mod, mod_name
 
 from .function_stubs import elementwise
 
+def raises(exceptions, function, message=''):
+    try:
+        function()
+    except exceptions:
+        return
+    except Exception as e:
+        if message:
+            raise AssertionError(f"Unexpected exception {e!r} (expected {exceptions}): {message}")
+        raise AssertionError(f"Unexpected exception {e!r} (expected {exceptions})")
+    raise AssertionError(message)
+
+def doesnt_raise(function, message=''):
+    if not callable(function):
+        raise ValueError("doesnt_raise should take a lambda")
+    try:
+        function()
+    except Exception as e:
+        if message:
+            raise AssertionError(f"Unexpected exception {e!r}: {message}")
+        raise AssertionError(f"Unexpected exception {e!r}")
+
 @pytest.mark.parametrize('name', elementwise._names)
 def test_has_names_elementwise(name):
     assert hasattr(mod, name), f"{mod_name} is missing the elementwise function {name}"
