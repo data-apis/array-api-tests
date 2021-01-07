6c7971d4b55a9b0c38e94306199e3b1506e09aca
Aaron Meurer <asmeurer@gmail.com>
2021-01-07 15:50:31 -0700

    Include floating point inputs in the arange() test

--- array_api_tests/test_creation_functions.py
+++ array_api_tests/test_creation_functions.py
@@ -1,13 +1,16 @@
-from ._array_module import arange
+from ._array_module import arange, ceil
 from .array_helpers import is_integer_dtype, dtype_ranges
 from .hypothesis_helpers import numeric_dtypes, MAX_ARRAY_SIZE
 
 from hypothesis import assume, given
-from hypothesis.strategies import integers, one_of, none
+from hypothesis.strategies import integers, floats, one_of, none
 
-@given(integers(-MAX_ARRAY_SIZE, MAX_ARRAY_SIZE),
-       one_of(integers(-MAX_ARRAY_SIZE, MAX_ARRAY_SIZE), none()),
-       one_of(integers(-MAX_ARRAY_SIZE, -1), integers(1, MAX_ARRAY_SIZE), none()), numeric_dtypes)
+int_range = integers(-MAX_ARRAY_SIZE, MAX_ARRAY_SIZE)
+float_range = floats(-MAX_ARRAY_SIZE, MAX_ARRAY_SIZE, allow_nan=False)
+@given(one_of(int_range, float_range),
+       one_of(int_range, float_range, none()),
+       one_of(int_range, float_range, none()).filter(lambda x: x != 0),
+       numeric_dtypes)
 def test_arange(start, stop, step, dtype):
     if dtype in dtype_ranges:
         m, M = dtype_ranges[dtype]
@@ -16,22 +19,35 @@ def test_arange(start, stop, step, dtype):
             or isinstance(step, int) and not (m <= step <= M)):
             assume(False)
 
+    all_int = (is_integer_dtype(dtype)
+               and isinstance(start, int)
+               and (stop is None or isinstance(stop, int))
+               and (step is None or isinstance(step, int)))
+
     if stop is None:
         # NB: "start" is really the stop
         # step is ignored in this case
         a = arange(start, dtype=dtype)
-        if is_integer_dtype(dtype):
+        if all_int:
             r = range(start)
     elif step is None:
         a = arange(start, stop, dtype=dtype)
-        if is_integer_dtype(dtype):
+        if all_int:
             r = range(start, stop)
     else:
         a = arange(start, stop, step, dtype=dtype)
-        if is_integer_dtype(dtype):
+        if all_int:
             r = range(start, stop, step)
     assert a.dtype == dtype, "arange() produced an incorrect dtype"
-    if is_integer_dtype(dtype):
+    assert a.ndim == 1, "arange() should return a 1-dimensional array"
+    if all_int:
         assert a.shape == (len(r),), "arange() produced incorrect shape"
         if len(r) <= MAX_ARRAY_SIZE:
             assert list(a) == list(r), "arange() produced incorrect values"
+    else:
+        # This is already implied by the len(r) test above
+        if (stop is not None
+            and step is not None
+            and (step > 0 and stop >= start
+                 or step < 0 and stop <= start)):
+            assert a.size == ceil((stop-start)/step), "arange() produced an array of the incorrect size"
