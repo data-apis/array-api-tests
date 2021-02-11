e8eb0c79f0981be6885d72ea24f6699219d4ee56
Aaron Meurer <asmeurer@gmail.com>
2021-02-11 14:39:36 -0700

    Add more boilerplate to the test_elementwise tests

--- array_api_tests/test_elementwise_functions.py
+++ array_api_tests/test_elementwise_functions.py
@@ -26,6 +26,8 @@ from .hypothesis_helpers import (integer_dtype_objects,
                                  boolean_dtypes, mutually_promotable_dtypes,
                                  array_scalars)
 
+from . import _array_module
+
 # integer_scalars = array_scalars(integer_dtypes)
 floating_scalars = array_scalars(floating_dtypes)
 numeric_scalars = array_scalars(numeric_dtypes)
@@ -110,245 +112,266 @@ input_types = {
     'tanh': 'floating',
     'trunc': 'numeric',
 }
-
 @given(numeric_scalars)
 def test_abs(x):
-    pass
+    a = _array_module.abs(x)
 
 @given(floating_scalars)
 def test_acos(x):
-    pass
+    a = _array_module.acos(x)
 
 @given(floating_scalars)
 def test_acosh(x):
-    pass
+    a = _array_module.acosh(x)
 
 @given(two_numeric_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_add(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.add(x1, x2)
 
 @given(floating_scalars)
 def test_asin(x):
-    pass
+    a = _array_module.asin(x)
 
 @given(floating_scalars)
 def test_asinh(x):
-    pass
+    a = _array_module.asinh(x)
 
 @given(floating_scalars)
 def test_atan(x):
-    pass
+    a = _array_module.atan(x)
 
 @given(two_floating_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_atan2(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.atan2(x1, x2)
 
 @given(floating_scalars)
 def test_atanh(x):
-    pass
+    a = _array_module.atanh(x)
 
 @given(two_integer_or_boolean_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_bitwise_and(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.bitwise_and(x1, x2)
 
 @given(two_integer_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_bitwise_left_shift(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.bitwise_left_shift(x1, x2)
 
 @given(integer_or_boolean_scalars)
 def test_bitwise_invert(x):
-    pass
+    a = _array_module.bitwise_invert(x)
 
 @given(two_integer_or_boolean_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_bitwise_or(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.bitwise_or(x1, x2)
 
 @given(two_integer_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_bitwise_right_shift(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.bitwise_right_shift(x1, x2)
 
 @given(two_integer_or_boolean_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_bitwise_xor(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.bitwise_xor(x1, x2)
 
 @given(numeric_scalars)
 def test_ceil(x):
-    pass
+    a = _array_module.ceil(x)
 
 @given(floating_scalars)
 def test_cos(x):
-    pass
+    a = _array_module.cos(x)
 
 @given(floating_scalars)
 def test_cosh(x):
-    pass
+    a = _array_module.cosh(x)
 
 @given(two_floating_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_divide(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.divide(x1, x2)
 
 @given(two_any_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_equal(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.equal(x1, x2)
 
 @given(floating_scalars)
 def test_exp(x):
-    pass
+    a = _array_module.exp(x)
 
 @given(floating_scalars)
 def test_expm1(x):
-    pass
+    a = _array_module.expm1(x)
 
 @given(numeric_scalars)
 def test_floor(x):
-    pass
+    a = _array_module.floor(x)
 
 @given(two_numeric_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_floor_divide(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.floor_divide(x1, x2)
 
 @given(two_numeric_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_greater(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.greater(x1, x2)
 
 @given(two_numeric_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_greater_equal(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.greater_equal(x1, x2)
 
 @given(numeric_scalars)
 def test_isfinite(x):
-    pass
+    a = _array_module.isfinite(x)
 
 @given(numeric_scalars)
 def test_isinf(x):
-    pass
+    a = _array_module.isinf(x)
 
 @given(numeric_scalars)
 def test_isnan(x):
-    pass
+    a = _array_module.isnan(x)
 
 @given(two_numeric_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_less(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.less(x1, x2)
 
 @given(two_numeric_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_less_equal(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.less_equal(x1, x2)
 
 @given(floating_scalars)
 def test_log(x):
-    pass
+    a = _array_module.log(x)
 
 @given(floating_scalars)
 def test_log1p(x):
-    pass
+    a = _array_module.log1p(x)
 
 @given(floating_scalars)
 def test_log2(x):
-    pass
+    a = _array_module.log2(x)
 
 @given(floating_scalars)
 def test_log10(x):
-    pass
+    a = _array_module.log10(x)
 
 @given(two_boolean_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_logical_and(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.logical_and(x1, x2)
 
 @given(boolean_scalars)
 def test_logical_not(x):
-    pass
+    a = _array_module.logical_not(x)
 
 @given(two_boolean_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_logical_or(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.logical_or(x1, x2)
 
 @given(two_boolean_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_logical_xor(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.logical_xor(x1, x2)
 
 @given(two_numeric_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_multiply(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.multiply(x1, x2)
 
 @given(numeric_scalars)
 def test_negative(x):
-    pass
+    a = _array_module.negative(x)
 
 @given(two_any_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_not_equal(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.not_equal(x1, x2)
 
 @given(numeric_scalars)
 def test_positive(x):
-    pass
+    a = _array_module.positive(x)
 
 @given(two_floating_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_pow(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.pow(x1, x2)
 
 @given(two_numeric_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_remainder(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.remainder(x1, x2)
 
 @given(numeric_scalars)
 def test_round(x):
-    pass
+    a = _array_module.round(x)
 
 @given(numeric_scalars)
 def test_sign(x):
-    pass
+    a = _array_module.sign(x)
 
 @given(floating_scalars)
 def test_sin(x):
-    pass
+    a = _array_module.sin(x)
 
 @given(floating_scalars)
 def test_sinh(x):
-    pass
+    a = _array_module.sinh(x)
 
 @given(numeric_scalars)
 def test_square(x):
-    pass
+    a = _array_module.square(x)
 
 @given(floating_scalars)
 def test_sqrt(x):
-    pass
+    a = _array_module.sqrt(x)
 
 @given(two_numeric_dtypes.flatmap(lambda i: two_array_scalars(*i)))
 def test_subtract(args):
     x1, x2 = args
     sanity_check(x1, x2)
+    a = _array_module.subtract(x1, x2)
 
 @given(floating_scalars)
 def test_tan(x):
-    pass
+    a = _array_module.tan(x)
 
 @given(floating_scalars)
 def test_tanh(x):
-    pass
+    a = _array_module.tanh(x)
 
 @given(numeric_scalars)
 def test_trunc(x):
-    pass
+    a = _array_module.trunc(x)
