0110d9cf7579a9a2254a40071ecd7c928459aced
Aaron Meurer <asmeurer@gmail.com>
2021-04-26 16:37:44 -0600

    Make List an importable type in the function stubs

--- generate_stubs.py
+++ generate_stubs.py
@@ -89,7 +89,7 @@ The type variables should be replaced with the actual types for a given
 library, e.g., for NumPy TypeVar('array') would be replaced with ndarray.
 """
 
-from typing import Literal, Optional, Tuple, Union, TypeVar
+from typing import List, Literal, Optional, Tuple, Union, TypeVar
 
 array = TypeVar('array')
 device = TypeVar('device')
@@ -98,7 +98,7 @@ SupportsDLPack = TypeVar('SupportsDLPack')
 SupportsBufferProtocol = TypeVar('SupportsBufferProtocol')
 PyCapsule = TypeVar('PyCapsule')
 
-__all__ = ['Literal', 'Optional', 'Tuple', 'Union', 'array', 'device',
+__all__ = ['List', 'Literal', 'Optional', 'Tuple', 'Union', 'array', 'device',
 'dtype', 'SupportsDLPack', 'SupportsBufferProtocol', 'PyCapsule']
 
 '''
