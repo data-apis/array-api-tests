99ae3f2ae0f9b26b979626701eddf026de1787be
Aaron Meurer <asmeurer@gmail.com>
2021-04-28 16:43:39 -0600

    Fix some issues in the test_constant tests

--- array_api_tests/test_constants.py
+++ array_api_tests/test_constants.py
@@ -1,5 +1,5 @@
 from ._array_module import (e, inf, nan, pi, equal, isnan, abs, full, float32,
-                            float64, less, isinf, greater)
+                            float64, less, isinf, greater, all)
 from .array_helpers import one
 
 def test_e():
@@ -8,7 +8,7 @@ def test_e():
 
     # We don't require any accuracy. This is just a smoke test to check that
     # 'e' is actually the constant e.
-    assert less(abs(E - 2.71), one((1,), dtype=float64)), "e is not the constant e"
+    assert all(less(abs(E - 2.71), one((1,), dtype=float64))), "e is not the constant e"
 
 def test_pi():
     # Check that pi acts as a scalar
@@ -16,23 +16,24 @@ def test_pi():
 
     # We don't require any accuracy. This is just a smoke test to check that
     # 'pi' is actually the constant π.
-    assert less(abs(PI - 3.14), one((1,), dtype=float64)), "pi is not the constant π"
+    assert all(less(abs(PI - 3.14), one((1,), dtype=float64))), "pi is not the constant π"
 
 def test_inf():
     # Check that inf acts as a scalar
     INF = full((1,), inf, dtype=float64)
+    zero = full((1,), 0.0, dtype=float64)
 
-    assert isinf(inf), "inf is not infinity"
-    assert isinf(INF), "inf is not infinity"
-    assert greater(inf, 0), "inf is not positive"
-    assert greater(INF, 0), "inf is not positive"
+    assert all(isinf(inf)), "inf is not infinity"
+    assert all(isinf(INF)), "inf is not infinity"
+    assert all(greater(inf, zero)), "inf is not positive"
+    assert all(greater(INF, zero)), "inf is not positive"
 
 def test_nan():
     # Check that nan acts as a scalar
     NAN = full((1,), nan, dtype=float64)
 
-    assert isnan(nan), "nan is not Not a Number"
-    assert isnan(NAN), "nan is not Not a Number"
+    assert all(isnan(nan)), "nan is not Not a Number"
+    assert all(isnan(NAN)), "nan is not Not a Number"
 
-    assert not equal(nan, nan), "nan should be unequal to itself"
-    assert not equal(NAN, NAN), "nan should be unequal to itself"
+    assert not all(equal(nan, nan)), "nan should be unequal to itself"
+    assert not all(equal(NAN, NAN)), "nan should be unequal to itself"
