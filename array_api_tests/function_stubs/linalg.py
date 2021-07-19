5ebd10b552f59641d563e763a1ac7a0c46d5172c
Aaron Meurer <asmeurer@gmail.com>
2021-07-19 16:21:30 -0600

    Update some type annotations

--- array_api_tests/function_stubs/linalg.py
+++ array_api_tests/function_stubs/linalg.py
@@ -65,7 +65,7 @@ def outer(x1: array, x2: array, /) -> array:
 def pinv(x: array, /, *, rtol: Optional[Union[float, array]] = None) -> array:
     pass
 
-def qr(x: array, /, *, mode: str = 'reduced') -> Tuple[array, array]:
+def qr(x: array, /, *, mode: Literal['reduced', 'complete'] = 'reduced') -> Tuple[array, array]:
     pass
 
 def slogdet(x: array, /) -> Tuple[array, array]:
@@ -74,13 +74,13 @@ def slogdet(x: array, /) -> Tuple[array, array]:
 def solve(x1: array, x2: array, /) -> array:
     pass
 
-def svd(x: array, /, *, full_matrices: bool = True) -> Union[array, Tuple[array, ...]]:
-    pass
-
-def tensordot(x1: array, x2: array, /, *, axes: Union[int, Tuple[Sequence[int], Sequence[int]]] = 2) -> array:
+def svd(x: array, /, *, full_matrices: bool = True) -> Tuple[array, array, array]:
     pass
 
 def svdvals(x: array, /) -> Union[array, Tuple[array, ...]]:
+    pass
+
+def tensordot(x1: array, x2: array, /, *, axes: Union[int, Tuple[Sequence[int], Sequence[int]]] = 2) -> array:
     pass
 
 def trace(x: array, /, *, axis1: int = 0, axis2: int = 1, offset: int = 0) -> array:
@@ -92,4 +92,4 @@ def transpose(x: array, /, *, axes: Optional[Tuple[int, ...]] = None) -> array:
 def vecdot(x1: array, x2: array, /, *, axis: Optional[int] = None) -> array:
     pass
 
-__all__ = ['cholesky', 'cross', 'det', 'diagonal', 'eig', 'eigh', 'eigvals', 'eigvalsh', 'einsum', 'inv', 'lstsq', 'matmul', 'matrix_power', 'matrix_rank', 'norm', 'outer', 'pinv', 'qr', 'slogdet', 'solve', 'svd', 'tensordot', 'svdvals', 'trace', 'transpose', 'vecdot']
+__all__ = ['cholesky', 'cross', 'det', 'diagonal', 'eig', 'eigh', 'eigvals', 'eigvalsh', 'einsum', 'inv', 'lstsq', 'matmul', 'matrix_power', 'matrix_rank', 'norm', 'outer', 'pinv', 'qr', 'slogdet', 'solve', 'svd', 'svdvals', 'tensordot', 'trace', 'transpose', 'vecdot']
