007175de26d56883430e6dcd443179152384d1b7
Matthew Barber <quitesimplymatt@gmail.com>
2021-08-16 17:11:36 +0100

    proof of concept `test_full_like`

--- array_api_tests/test_creation_functions.py
+++ array_api_tests/test_creation_functions.py
@@ -1,13 +1,14 @@
-from ._array_module import (asarray, arange, ceil, empty, eye, full,
-equal, all, linspace, ones, zeros, isnan)
+from hypothesis.strategies._internal.core import sampled_from
+from ._array_module import (asarray, arange, ceil, empty, eye, full, full_like,
+                            equal, all, linspace, ones, zeros, isnan)
 from .array_helpers import (is_integer_dtype, dtype_ranges,
                             assert_exactly_equal, isintegral, is_float_dtype)
 from .hypothesis_helpers import (numeric_dtypes, dtypes, MAX_ARRAY_SIZE,
                                  shapes, sizes, sqrt_sizes, shared_dtypes,
-                                 scalars)
+                                 scalars, xps)
 
 from hypothesis import assume, given
-from hypothesis.strategies import integers, floats, one_of, none, booleans, just
+from hypothesis.strategies import integers, floats, one_of, none, booleans, just, data
 
 int_range = integers(-MAX_ARRAY_SIZE, MAX_ARRAY_SIZE)
 float_range = floats(-MAX_ARRAY_SIZE, MAX_ARRAY_SIZE,
@@ -125,14 +126,32 @@ def test_full(shape, fill_value, dtype):
         assert a.dtype == dtype
 
     assert a.shape == shape, "full() produced an array with incorrect shape"
+
     if is_float_dtype(a.dtype) and isnan(asarray(fill_value)):
         assert all(isnan(a)), "full() array did not equal the fill value"
     else:
         assert all(equal(a, asarray(fill_value, **kwargs))), "full() array did not equal the fill value"
 
-# TODO: implement full_like (requires hypothesis arrays support)
-def test_full_like():
-    pass
+@given(xps.scalar_dtypes(), xps.array_shapes(), data())
+def test_full_like(dtype, shape, data):
+    a = data.draw(xps.arrays(dtype, shape))
+    fill_value = data.draw(xps.from_dtype(dtype))
+    kwargs = data.draw(sampled_from([{}, {'dtype': dtype}]))
+
+    a_like = full_like(a, fill_value, **kwargs)
+
+    if dtype is None:
+        pass # TODO: Should it actually match the fill_value?
+    else:
+        assert a_like.dtype == dtype
+
+    assert a_like.shape == shape, "full_like() produced an array with incorrect shape"
+
+    if is_float_dtype(a_like.dtype) and isnan(asarray(fill_value)):
+        assert all(isnan(a_like)), "full_like() array did not equal the fill value"
+    else:
+        assert all(equal(a_like, asarray(fill_value, dtype=dtype))), "full_like() array did not equal the fill value"
+
 
 @given(scalars(shared_dtypes, finite=True),
        scalars(shared_dtypes, finite=True),
