92a65da60555e1aaccf8ac8ed2160705140e5e1d
Aaron Meurer <asmeurer@gmail.com>
2021-10-04 16:40:11 -0600

    Add stubs for the linalg tests
    Generated with
    
    import inspect
    from array_api_tests.function_stubs import linalg
    
    funcs = [getattr(linalg, n) for n in linalg.__all__]
    
    for f in funcs:
        s = inspect.getfullargspec(f)
        args = call_args = ', '.join(s.args)
        if s.kwonlyargs:
           args += ', kw'
           call_args += ', **kw'
        givens = ''
        for x in s.args:
            givens += f'    {x}=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),\n'
        if s.kwonlyargs:
            kwonly = ', '.join([f'{kw}=...' for kw in s.kwonlyargs])
            givens += f'    kw=kwargs({kwonly})\n'
        print(f"""\
    @given(
    {givens})
    def test_{f.__name__}({args}):
        # res = _array_module.linalg.{f.__name__}({call_args})
        pass
    """)

---
+++ array_api_tests/test_linalg.py
@@ -0,0 +1,203 @@
+"""
+Tests for linalg functions
+
+https://data-apis.org/array-api/latest/API_specification/linear_algebra_functions.html
+
+and
+
+https://data-apis.org/array-api/latest/extensions/linear_algebra_functions.html
+
+Note: this file currently mixes both the required linear algebra functions and
+functions from the linalg extension. The functions in the latter are not
+required, but we don't yet have a clean way to disable only those tests (see https://github.com/data-apis/array-api-tests/issues/25).
+
+"""
+
+from hypothesis import given
+
+from .hypothesis_helpers import xps, shapes, kwargs, none
+
+from . import _array_module
+
+# Standin strategy for not yet implemented tests
+todo = none()
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    kw=kwargs(upper=todo)
+)
+def test_cholesky(x, kw):
+    # res = _array_module.linalg.cholesky(x, **kw)
+    pass
+
+@given(
+    x1=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    x2=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    kw=kwargs(axis=todo)
+)
+def test_cross(x1, x2, kw):
+    # res = _array_module.linalg.cross(x1, x2, **kw)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+)
+def test_det(x):
+    # res = _array_module.linalg.det(x)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    kw=kwargs(offset=todo)
+)
+def test_diagonal(x, kw):
+    # res = _array_module.linalg.diagonal(x, **kw)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+)
+def test_eigh(x):
+    # res = _array_module.linalg.eigh(x)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+)
+def test_eigvalsh(x):
+    # res = _array_module.linalg.eigvalsh(x)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+)
+def test_inv(x):
+    # res = _array_module.linalg.inv(x)
+    pass
+
+@given(
+    x1=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    x2=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+)
+def test_matmul(x1, x2):
+    # res = _array_module.linalg.matmul(x1, x2)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    kw=kwargs(axis=todo, keepdims=todo, ord=todo)
+)
+def test_matrix_norm(x, kw):
+    # res = _array_module.linalg.matrix_norm(x, **kw)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    n=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+)
+def test_matrix_power(x, n):
+    # res = _array_module.linalg.matrix_power(x, n)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    kw=kwargs(rtol=todo)
+)
+def test_matrix_rank(x, kw):
+    # res = _array_module.linalg.matrix_rank(x, **kw)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+)
+def test_matrix_transpose(x):
+    # res = _array_module.linalg.matrix_transpose(x)
+    pass
+
+@given(
+    x1=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    x2=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+)
+def test_outer(x1, x2):
+    # res = _array_module.linalg.outer(x1, x2)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    kw=kwargs(rtol=todo)
+)
+def test_pinv(x, kw):
+    # res = _array_module.linalg.pinv(x, **kw)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    kw=kwargs(mode=todo)
+)
+def test_qr(x, kw):
+    # res = _array_module.linalg.qr(x, **kw)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+)
+def test_slogdet(x):
+    # res = _array_module.linalg.slogdet(x)
+    pass
+
+@given(
+    x1=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    x2=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+)
+def test_solve(x1, x2):
+    # res = _array_module.linalg.solve(x1, x2)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    kw=kwargs(full_matrices=todo)
+)
+def test_svd(x, kw):
+    # res = _array_module.linalg.svd(x, **kw)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+)
+def test_svdvals(x):
+    # res = _array_module.linalg.svdvals(x)
+    pass
+
+@given(
+    x1=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    x2=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    kw=kwargs(axes=todo)
+)
+def test_tensordot(x1, x2, kw):
+    # res = _array_module.linalg.tensordot(x1, x2, **kw)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    kw=kwargs(offset=todo)
+)
+def test_trace(x, kw):
+    # res = _array_module.linalg.trace(x, **kw)
+    pass
+
+@given(
+    x1=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    x2=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    kw=kwargs(axis=todo)
+)
+def test_vecdot(x1, x2, kw):
+    # res = _array_module.linalg.vecdot(x1, x2, **kw)
+    pass
+
+@given(
+    x=xps.arrays(dtype=xps.floating_dtypes(), shape=shapes),
+    kw=kwargs(axis=todo, keepdims=todo, ord=todo)
+)
+def test_vector_norm(x, kw):
+    # res = _array_module.linalg.vector_norm(x, **kw)
+    pass
