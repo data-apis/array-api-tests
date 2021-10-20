0d19396cefc42b83d85de010c6221abcbc33a3e7
Matthew <quitesimplymatt@gmail.com>
2021-10-20 09:10:04 +0100

    Add `--enable-extension` option

--- array_api_tests/conftest.py
+++ array_api_tests/conftest.py
@@ -9,9 +9,17 @@ from ._array_module import _UndefinedStub
 def pytest_addoption(parser):
     parser.addoption(
         '--disable-extension',
+        metavar='ext',
         nargs='+',
         default=[],
         help='disable testing for Array API extension(s)',
+    )
+    parser.addoption(
+        '--enable-extension',
+        metavar='ext',
+        nargs='+',
+        default=[],
+        help='enable testing for Array API extension(s)',
     )
 
 
@@ -31,6 +39,10 @@ def xp_has_ext(ext: str) -> bool:
 
 def pytest_collection_modifyitems(config, items):
     disabled_exts = config.getoption('--disable-extension')
+    enabled_exts = config.getoption('--enable-extension')
+    for ext in disabled_exts:
+        if ext in enabled_exts:
+            raise ValueError(f'{ext=} both enabled and disabled')
     for item in items:
         try:
             ext_mark = next(
@@ -41,5 +53,5 @@ def pytest_collection_modifyitems(config, items):
         ext = ext_mark.args[0]
         if ext in disabled_exts:
             item.add_marker(mark.skip(reason=f'{ext} disabled in --disable-extensions'))
-        elif not xp_has_ext(ext):
+        elif not ext in enabled_exts and not xp_has_ext(ext):
             item.add_marker(mark.skip(reason=f'{ext} not found in array module'))
