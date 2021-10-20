a860c46da14eb749187018b815043dfabf849c60
Matthew <quitesimplymatt@gmail.com>
2021-10-20 10:39:39 +0100

    Read `xfails.txt` for xfailing tests, update NumPy workflow
    And merges both `conftest.py` files together

--- array_api_tests/meta/test_utils.py
+++ array_api_tests/meta/test_utils.py
@@ -1,5 +1,4 @@
 from ..test_signatures import extension_module
-from ..conftest import xp_has_ext
 
 
 def test_extension_module_is_extension():
@@ -8,7 +7,3 @@ def test_extension_module_is_extension():
 
 def test_extension_func_is_not_extension():
     assert not extension_module('linalg.cross')
-
-
-def test_xp_has_ext():
-    assert not xp_has_ext('nonexistent_extension')
--- conftest.py
+++ conftest.py
@@ -1,4 +1,12 @@
+from functools import lru_cache
+from pathlib import Path
+
+from pytest import mark
 from hypothesis import settings
+
+from array_api_tests import _array_module as xp
+from array_api_tests._array_module import _UndefinedStub
+
 
 settings.register_profile('xp_default', deadline=800)
 
@@ -20,9 +28,28 @@ def pytest_addoption(parser):
         action='store_true',
         help='disable the Hypothesis deadline',
     )
+    # enable/disable extensions
+    parser.addoption(
+        '--enable-extension',
+        metavar='ext',
+        nargs='+',
+        default=[],
+        help='enable testing for Array API extension(s)',
+    )
+    parser.addoption(
+        '--disable-extension',
+        metavar='ext',
+        nargs='+',
+        default=[],
+        help='disable testing for Array API extension(s)',
+    )
 
 
 def pytest_configure(config):
+    config.addinivalue_line(
+        'markers', 'xp_extension(ext): tests an Array API extension'
+    )
+    # Hypothesis
     hypothesis_max_examples = config.getoption('--hypothesis-max-examples')
     disable_deadline = config.getoption('--hypothesis-disable-deadline')
     profile_settings = {}
@@ -35,3 +62,45 @@ def pytest_configure(config):
         settings.load_profile('xp_override')
     else:
         settings.load_profile('xp_default')
+
+
+@lru_cache
+def xp_has_ext(ext: str) -> bool:
+    try:
+        return not isinstance(getattr(xp, ext), _UndefinedStub)
+    except AttributeError:
+        return False
+
+
+xfail_ids = []
+xfails_path = Path(__file__).parent / 'xfails.txt'
+if xfails_path.exists():
+    with open(xfails_path) as f:
+        for line in f:
+            if line.startswith('test'):
+                id_ = line.strip('\n')
+                xfail_ids.append(id_)
+
+
+def pytest_collection_modifyitems(config, items):
+    enabled_exts = config.getoption('--enable-extension')
+    disabled_exts = config.getoption('--disable-extension')
+    for ext in enabled_exts:
+        if ext in disabled_exts:
+            raise ValueError(f'{ext=} both enabled and disabled')
+    for item in items:
+        # enable/disable extensions
+        try:
+            ext_mark = next(m for m in item.iter_markers() if m.name == 'xp_extension')
+            ext = ext_mark.args[0]
+            if ext in disabled_exts:
+                item.add_marker(
+                    mark.skip(reason=f'{ext} disabled in --disable-extensions')
+                )
+            elif not ext in enabled_exts and not xp_has_ext(ext):
+                item.add_marker(mark.skip(reason=f'{ext} not found in array module'))
+        except StopIteration:
+            pass
+        # workflow xfail_ids
+        if item.nodeid in xfail_ids:
+            item.add_marker(mark.xfail(reason='xfails.txt'))
