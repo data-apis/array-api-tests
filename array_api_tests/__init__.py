0938424895e0edd98386a3265cfc1b3bac57c106
Matthew <quitesimplymatt@gmail.com>
2021-10-29 10:57:34 +0100

    Alias `mod` as `_xp`, clean `__init__` namespace

--- array_api_tests/__init__.py
+++ array_api_tests/__init__.py
@@ -1,6 +1,10 @@
 from hypothesis.extra.array_api import make_strategies_namespace
 
-from ._array_module import mod as xp
+from ._array_module import mod as _xp
 
 
-xps = make_strategies_namespace(xp)
+xps = make_strategies_namespace(_xp)
+
+
+del _xp
+del make_strategies_namespace
