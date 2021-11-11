30a072163576a6664c94b93dd4437432f4fbeec7
Matthew <quitesimplymatt@gmail.com>
2021-11-11 18:57:49 +0000

    Improve `min()`/`max()` tests

--- array_api_tests/test_statistical_functions.py
+++ array_api_tests/test_statistical_functions.py
@@ -1,22 +1,108 @@
+import math
+
 from hypothesis import given
+from hypothesis import strategies as st
 
 from . import _array_module as xp
+from . import array_helpers as ah
+from . import dtype_helpers as dh
 from . import hypothesis_helpers as hh
+from . import pytest_helpers as ph
 from . import xps
 
 
-# TODO: generate kwargs
-@given(xps.arrays(dtype=xps.numeric_dtypes(), shape=hh.shapes(min_side=1)))
-def test_min(x):
-    xp.min(x)
-    # TODO
+@given(
+    x=xps.arrays(dtype=xps.numeric_dtypes(), shape=hh.shapes(min_side=1)),
+    data=st.data(),
+)
+def test_min(x, data):
+    axis_strats = [st.none()]
+    if x.shape != ():
+        axis_strats.append(
+            st.integers(-x.ndim, x.ndim - 1) | xps.valid_tuple_axes(x.ndim)
+        )
+    kw = data.draw(
+        hh.kwargs(axis=st.one_of(axis_strats), keepdims=st.booleans()), label="kw"
+    )
+
+    out = xp.min(x, **kw)
+
+    ph.assert_dtype("min", x.dtype, out.dtype)
+
+    f_func = f"min({ph.fmt_kw(kw)})"
+
+    # TODO: support axis
+    if kw.get("axis") is None:
+        keepdims = kw.get("keepdims", False)
+        if keepdims:
+            idx = tuple(1 for _ in x.shape)
+            msg = f"{out.shape=}, should be reduced dimension {idx} [{f_func}]"
+            assert out.shape == idx
+        else:
+            ph.assert_shape("min", out.shape, (), **kw)
+
+        # TODO: figure out NaN behaviour
+        if dh.is_int_dtype(x.dtype) or not xp.any(xp.isnan(x)):
+            _out = xp.reshape(out, ()) if keepdims else out
+            scalar_type = dh.get_scalar_type(out.dtype)
+            elements = []
+            for idx in ah.ndindex(x.shape):
+                s = scalar_type(x[idx])
+                elements.append(s)
+            min_ = scalar_type(_out)
+            expected = min(elements)
+            msg = f"out={min_}, should be {expected} [{f_func}]"
+            if math.isnan(min_):
+                assert math.isnan(expected), msg
+            else:
+                assert min_ == expected, msg
 
 
-# TODO: generate kwargs
-@given(xps.arrays(dtype=xps.numeric_dtypes(), shape=hh.shapes(min_side=1)))
-def test_max(x):
-    xp.max(x)
-    # TODO
+@given(
+    x=xps.arrays(dtype=xps.numeric_dtypes(), shape=hh.shapes(min_side=1)),
+    data=st.data(),
+)
+def test_max(x, data):
+    axis_strats = [st.none()]
+    if x.shape != ():
+        axis_strats.append(
+            st.integers(-x.ndim, x.ndim - 1) | xps.valid_tuple_axes(x.ndim)
+        )
+    kw = data.draw(
+        hh.kwargs(axis=st.one_of(axis_strats), keepdims=st.booleans()), label="kw"
+    )
+
+    out = xp.max(x, **kw)
+
+    ph.assert_dtype("max", x.dtype, out.dtype)
+
+    f_func = f"max({ph.fmt_kw(kw)})"
+
+    # TODO: support axis
+    if kw.get("axis") is None:
+        keepdims = kw.get("keepdims", False)
+        if keepdims:
+            idx = tuple(1 for _ in x.shape)
+            msg = f"{out.shape=}, should be reduced dimension {idx} [{f_func}]"
+            assert out.shape == idx
+        else:
+            ph.assert_shape("max", out.shape, (), **kw)
+
+        # TODO: figure out NaN behaviour
+        if dh.is_int_dtype(x.dtype) or not xp.any(xp.isnan(x)):
+            _out = xp.reshape(out, ()) if keepdims else out
+            scalar_type = dh.get_scalar_type(out.dtype)
+            elements = []
+            for idx in ah.ndindex(x.shape):
+                s = scalar_type(x[idx])
+                elements.append(s)
+            max_ = scalar_type(_out)
+            expected = max(elements)
+            msg = f"out={max_}, should be {expected} [{f_func}]"
+            if math.isnan(max_):
+                assert math.isnan(expected), msg
+            else:
+                assert max_ == expected, msg
 
 
 # TODO: generate kwargs
