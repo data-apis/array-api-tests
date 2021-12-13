d594ff5430a0ef086a9d595bc6cfd2bcccc6e00a
Matthew <quitesimplymatt@gmail.com>
2021-12-13 13:15:38 +0000

    Cover everything in `test_unique_values()`

--- array_api_tests/test_set_functions.py
+++ array_api_tests/test_set_functions.py
@@ -1,7 +1,12 @@
-from hypothesis import given
+import math
+
+from hypothesis import assume, given
 
 from . import _array_module as xp
+from . import array_helpers as ah
+from . import dtype_helpers as dh
 from . import hypothesis_helpers as hh
+from . import pytest_helpers as ph
 from . import xps
 
 
@@ -23,7 +28,25 @@ def test_unique_inverse(x):
     # TODO
 
 
-@given(xps.arrays(dtype=xps.scalar_dtypes(), shape=hh.shapes()))
+@given(xps.arrays(dtype=xps.scalar_dtypes(), shape=hh.shapes(min_side=1)))
 def test_unique_values(x):
-    xp.unique_values(x)
-    # TODO
+    out = xp.unique_values(x)
+    ph.assert_dtype("unique_values", x.dtype, out.dtype)
+    scalar_type = dh.get_scalar_type(x.dtype)
+    distinct = set(scalar_type(x[idx]) for idx in ah.ndindex(x.shape))
+    vals_idx = {}
+    nans = 0
+    for idx in ah.ndindex(out.shape):
+        val = scalar_type(out[idx])
+        if math.isnan(val):
+            nans += 1
+        else:
+            assert val in distinct, f"out[{idx}]={val}, but {val} not in input array"
+            assert (
+                val not in vals_idx.keys()
+            ), f"out[{idx}]={val}, but {val} is also in out[{vals_idx[val]}]"
+            vals_idx[val] = idx
+    if dh.is_float_dtype(out.dtype):
+        assume(x.size <= 128)  # may not be representable
+        expected_nans = xp.sum(xp.astype(xp.isnan(x), xp.uint8))
+        assert nans == expected_nans, f"{nans} NaNs in out, expected {expected_nans}"
