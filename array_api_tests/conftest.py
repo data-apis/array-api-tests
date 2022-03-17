8a59494a7a77eca3edbeaf8e351464970b073967
Matthew Barber <quitesimplymatt@gmail.com>
2022-03-17 09:51:26 +0000

    Skip faulty int type promotion tests for pow-related functions

---
+++ array_api_tests/conftest.py
@@ -0,0 +1,17 @@
+import re
+
+import pytest
+
+r_int_pow_promotion = re.compile(r"test.+promotion\[.*pow.*\(u?int.+\]")
+
+
+def pytest_collection_modifyitems(config, items):
+    """Skips the faulty integer type promotion tests for pow-related functions"""
+    for item in items:
+        print(item.name)
+        if r_int_pow_promotion.match(item.name):
+            item.add_marker(
+                pytest.mark.skip(
+                    reason="faulty test logic - negative exponents generated"
+                )
+            )
