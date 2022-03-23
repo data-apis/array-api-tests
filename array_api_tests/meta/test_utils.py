a4999e2bf012cfef70ffc6668d2e0d0328d0097a
Matthew Barber <quitesimplymatt@gmail.com>
2022-03-23 12:48:46 +0000

    Remove defunct `extension_module()` meta tests

--- array_api_tests/meta/test_utils.py
+++ array_api_tests/meta/test_utils.py
@@ -13,15 +13,6 @@ from ..test_operators_and_elementwise_functions import (
     oneway_broadcastable_shapes,
     oneway_promotable_dtypes,
 )
-from ..test_signatures import extension_module
-
-
-def test_extension_module_is_extension():
-    assert extension_module("linalg")
-
-
-def test_extension_func_is_not_extension():
-    assert not extension_module("linalg.cross")
 
 
 @pytest.mark.parametrize(
