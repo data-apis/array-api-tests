516817655a5483f98b3f53c41a80917d281d4c72
Aaron Meurer <asmeurer@gmail.com>
2022-09-06 16:55:59 -0600

    Allow specifying the report file in verify_report.py

--- verify_report.py
+++ verify_report.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
+import sys
 import json
+
 import jsonschema
 
 def main():
+    if len(sys.argv) == 1:
+        report_file = '.report.json'
+    elif len(sys.argv) == 2:
+        report_file = sys.argv[1]
+    else:
+        sys.exit("Usage: verify_report.py [json_report_file]")
+
     schema = json.load(open('report.schema.json'))
 
     jsonschema.Validator.check_schema(schema)
 
-    with open('.report.json') as f:
+    with open(report_file) as f:
         report = json.load(f)
 
     jsonschema.validate(report, schema)
