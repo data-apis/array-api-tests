ddcbce0366bb1352eb774856691660943d2821f6
Jake VanderPlas <jakevdp@google.com>
2023-11-14 21:22:00 -0800

    Update to most recent array-api commit

--- array_api_tests/stubs.py
+++ array_api_tests/stubs.py
@@ -15,20 +15,22 @@ __all__ = [
     "extension_to_funcs",
 ]
 
+spec_version = "2022.12"
+spec_module = "_" + spec_version.replace('.', '_')
 
-spec_dir = Path(__file__).parent.parent / "array-api" / "spec" / "API_specification"
+spec_dir = Path(__file__).parent.parent / "array-api" / "spec" / spec_version / "API_specification"
 assert spec_dir.exists(), f"{spec_dir} not found - try `git submodule update --init`"
-sigs_dir = spec_dir / "signatures"
+sigs_dir = Path(__file__).parent.parent / "array-api" / "src" / "array_api_stubs" / spec_module
 assert sigs_dir.exists()
 
-spec_abs_path: str = str(spec_dir.resolve())
-sys.path.append(spec_abs_path)
-assert find_spec("signatures") is not None
+sigs_abs_path: str = str(sigs_dir.parent.parent.resolve())
+sys.path.append(sigs_abs_path)
+assert find_spec(f"array_api_stubs.{spec_module}") is not None
 
 name_to_mod: Dict[str, ModuleType] = {}
 for path in sigs_dir.glob("*.py"):
     name = path.name.replace(".py", "")
-    name_to_mod[name] = import_module(f"signatures.{name}")
+    name_to_mod[name] = import_module(f"array_api_stubs.{spec_module}.{name}")
 
 array = name_to_mod["array_object"].array
 array_methods = [
