ca48dd35ea140b62c24c29ec161c9115ead0cb60
Matthew Barber <quitesimplymatt@gmail.com>
2023-11-24 13:38:58 +0000

    Fix bug collecting all special cases

--- array_api_tests/test_special_cases.py
+++ array_api_tests/test_special_cases.py
@@ -1174,8 +1174,8 @@ for stub in category_to_funcs["elementwise"]:
                 op = getattr(operator, op_name)
                 name_to_func[op_name] = op
                 # We collect inplace operator test cases seperately
-                if stub.__name__ == "equal":
-                    break
+                if "equal" in stub.__name__:
+                    continue
                 iop_name = "__i" + op_name[2:]
                 iop = getattr(operator, iop_name)
                 for case in cases:
