48a8e5866120feb0c14e302202463d95fa6b439c
Matthew Barber <quitesimplymatt@gmail.com>
2023-11-24 10:06:54 +0000

    Remove debug statement

--- array_api_tests/test_special_cases.py
+++ array_api_tests/test_special_cases.py
@@ -1128,9 +1128,6 @@ binary_params = []
 iop_params = []
 func_to_op: Dict[str, str] = {v: k for k, v in dh.op_to_func.items()}
 for stub in category_to_funcs["elementwise"]:
-    # if stub.__name__ == "abs":
-    #     import ipdb; ipdb.set_trace()
-
     if stub.__doc__ is None:
         warn(f"{stub.__name__}() stub has no docstring")
         continue
