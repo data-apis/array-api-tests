e7b55994bc5ff7a1c856316c804642244f7874b6
Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>
2026-08-20 17:15:31 +0530

    use another try block
    Signed-off-by: Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>

--- array_api_tests/dtype_helpers.py
+++ array_api_tests/dtype_helpers.py
@@ -205,6 +205,9 @@ def is_device_dlpack_compatible(device):
 
     try:
         x = xp.empty(2, device=device)
+    except:
+        x = xp.empty(2)
+    try:
         x.__dlpack_device__()
     except:
         # case in point: torch.device(type="meta") raises
