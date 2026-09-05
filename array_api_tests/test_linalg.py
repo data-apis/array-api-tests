b676d24809964240abd8e51b8f42b5c939dc7f32
Matt Haberland <matt.haberland@gmail.com>
2026-09-05 12:33:48 -0700

    Use stack to construct the expected transpose

--- array_api_tests/test_linalg.py
+++ array_api_tests/test_linalg.py
@@ -517,12 +517,10 @@ def _test_matrix_transpose(namespace, x):
     matrix_transpose = namespace.matrix_transpose
     res = matrix_transpose(x)
     def true_val(a):
-        expected = _array_module.empty((a.shape[1], a.shape[0]),
-                                       dtype=a.dtype, device=a.device)
-        for i in range(a.shape[0]):
-            for j in range(a.shape[1]):
-                expected[j, i] = a[i, j]
-        return expected
+        if 0 in a.shape:
+            return xp.empty((a.shape[1], a.shape[0]),
+                            dtype=a.dtype, device=a.device)
+        return xp.stack([a[i, :] for i in range(a.shape[0])], axis=1)
 
     shape = list(x.shape)
     shape[-1], shape[-2] = shape[-2], shape[-1]
